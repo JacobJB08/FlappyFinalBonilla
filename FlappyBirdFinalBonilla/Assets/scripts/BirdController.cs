28a15fc27888e536cf379b88e499f46dc392ad65
JacobJB08 <721378@student.dusd.net>
2022-12-16 09:57:31 -0800

    Lot's of Work
    Added anims, and basic scripts.

---
+++ FlappyBirdFinalBonilla/Assets/scripts/BirdController.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdController : MonoBehaviour
+{
+    public float upForce = 200f;
+    private bool isDead = false;
+    private Rigidbody2D rb2d;
+    private Animator anim;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (isDead == false)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                rb2d.velocity = Vector2.zero;
+                rb2d.AddForce(new Vector2(0, upForce));
+                anim.SetTrigger("Flap");
+            }
+        }
+    }
+
+    void OnCollisionEnter2D ()
+    {
+        isDead = true;
+        anim.SetTrigger("Die");
+    }
+}
