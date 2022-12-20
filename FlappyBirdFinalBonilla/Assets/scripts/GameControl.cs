a70a139efea97ca1e7cd93707a5df85d3cc6cb5e
JacobJB08 <721378@student.dusd.net>
2022-12-20 10:38:04 -0800

    stuff idk

---
+++ FlappyBirdFinalBonilla/Assets/scripts/GameControl.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameControl : MonoBehaviour
+{
+    public float scrollSpeed = -1.5f;
+    public static GameControl instance;
+    public GameObject gameOverText;
+    public bool gameOver = false;
+    private int score = 0;
+    public Text scoreText;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameOver == true && Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+
+    public void BirdDied()
+    {
+        gameOverText.SetActive(true);
+        gameOver = true;
+    }
+
+    public void BirdScored()
+    {
+        if (gameOver)
+        {
+            return;
+        }
+        score++;
+        scoreText.text = "Score: " + score.ToString();
+    }
+}
